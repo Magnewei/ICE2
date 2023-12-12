5e52500a02cb8ccde1f7271f6c1c0af690bb2716
Magi2106 <144230160+Magi2106@users.noreply.github.com>
2023-12-12 17:10:04 +0100

    gae

--- src/main/java/com/example/ice/Main.java
+++ src/main/java/com/example/ice/Main.java
@@ -16,7 +16,7 @@ public class Main extends Application {
     }
 
     public void start(Stage primaryStage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("BattleSim.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("Menu.fxml"));
         Pane root = fxmlLoader.load();
         Scene scene = new Scene(root);
         primaryStage.setTitle("Datamon.exe");
